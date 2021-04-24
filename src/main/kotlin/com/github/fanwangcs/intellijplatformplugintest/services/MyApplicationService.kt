01bbc4edce6ddbda439a44c0998562c0cd1811ff
GitHub Action <action@github.com>
2021-04-24 13:49:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fanwangcs/intellijplatformplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fanwangcs.intellijplatformplugintest.services
+
+import com.github.fanwangcs.intellijplatformplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
